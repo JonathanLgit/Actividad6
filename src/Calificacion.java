6a31694482956e89fbfb7e308b8cb9a4487163c0
Jonathan <jolm.020@outlook.com>
2024-03-01 19:57:31 -0600

    update

--- src/Calificacion.java
+++ src/Calificacion.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Calificacion {
+public class Calificacion {
+    private String nombre;
+    private int[] arr;
+
+    public Calificacion(String nombre, int[] arr) {
+        this.nombre = nombre;
+        this.arr = arr;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public int calcularPromedio (){
+        return (arr[0]+arr[1]+arr[2]+arr[3]+arr[4])/arr.length;
+    }
+
+    public char obtenerCalificacion (){
+        int promedio = calcularPromedio();
+        char calificacion = 'n';
+
+        if (promedio <= 50) {
+            calificacion = 'F';
+        }
+        else if (promedio <= 60) {
+            calificacion = 'E';
+        }
+        else if (promedio <= 70) {
+            calificacion = 'D';
+        }
+        else if (promedio <= 80) {
+            calificacion = 'C';
+        }
+        else if (promedio <= 90) {
+            calificacion = 'B';
+        }
+        else if (promedio <= 100){
+            calificacion = 'A';
+        }
+        return calificacion;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calificacion alumno1 = new Calificacion("Jonathan",new int[]{90,50,70,80,75});
+
+
+        System.out.println("Nombre del estudiante: " + alumno1.getNombre());
+
+        int[] calificaciones1 = alumno1.getArr();
+        for (int i = 0; i < calificaciones1.length; i++) {
+            System.out.println("Calificación " + (i + 1) + ": " + calificaciones1[i]);
+        }
+
+        System.out.println("Promedio: " + alumno1.calcularPromedio());
+        System.out.println("Calificacion: " + alumno1.obtenerCalificacion());
     }
 }
